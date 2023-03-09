1a065fcae048436f64d168b17425739397ecc3d6
Benjamin Bailon <benjamin.bailon@outlook.com>
2023-03-08 22:41:47 -0600

    M08 realizado

--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -1,60 +1,82 @@
 /*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
 
 function mayuscula(nombre) {
-   // Esta función recibe un nombre (string).
-   // Debe devolver el mismo nombre, pero con la primera letra en mayúscula.
-   // [Ejemplo]: "mario" ----> "Mario".
-   // Tu código:
+  // Esta función recibe un nombre (string).
+  // Debe devolver el mismo nombre, pero con la primera letra en mayúscula.
+  // [Ejemplo]: "mario" ----> "Mario".
+  // Tu código:
+  return nombre.charAt(0).toUpperCase() + nombre.slice(1);
 }
 
 function invocarCallback(cb) {
-   // Invoca/ejecuta el callback `cb`.
-   // [NOTA]: no debes reotrnar nada.
-   // Tu código:
+  // Invoca/ejecuta el callback `cb`.
+  // [NOTA]: no debes reotrnar nada.
+  // Tu código:
+  cb();
 }
 
 function operacionMatematica(num1, num2, cb) {
-   // En este ejercicio recibirás dos números y un callback.
-   // El callback realiza una operación matemática, por lo que necesita de los dos números.
-   // Retorna el resultado del callback pasándole como parámetros los números.
-   // Tu código:
+  // En este ejercicio recibirás dos números y un callback.
+  // El callback realiza una operación matemática, por lo que necesita de los dos números.
+  // Retorna el resultado del callback pasándole como parámetros los números.
+  // Tu código:
+  return cb(num1, num2);
 }
 
 function sumarArray(arrayOfNumbers, cb) {
-   // Recibes un arreglo de números y un callback.
-   // Suma todos los números del arreglo.
-   // Este resultado debes pasárselo como argumento al callback recibido.
-   // [NOTA]: no debes reotrnar nada.
-   // Tu código:
+  // Recibes un arreglo de números y un callback.
+  // Suma todos los números del arreglo.
+  // Este resultado debes pasárselo como argumento al callback recibido.
+  // [NOTA]: no debes reotrnar nada.
+  // Tu código:
+  let suma = 0;
+  for (let i = 0; i < arrayOfNumbers.length; i++) {
+    suma += arrayOfNumbers[i];
+  }
+  cb(suma);
 }
 
 function forEach(array, cb) {
-   // Recibes un arreglo y un callback.
-   // Debes iterar sobre el arreglo, y por cada elemento ejecutar el callback.
-   // Debes pasarle el elemento como argumento al callback.
-   // Tu código:
+  // Recibes un arreglo y un callback.
+  // Debes iterar sobre el arreglo, y por cada elemento ejecutar el callback.
+  // Debes pasarle el elemento como argumento al callback.
+  // Tu código:
+  array.forEach((value) => cb(value));
 }
 
 function map(array, cb) {
-   // Debes iterar sobre el arreglo, y cada elemento pasárselo como arguemento al callback.
-   // Tiene que guardar el resultado devuelto por el callback en cada elemento dentro de un nuevo arreglo.
-   // Retorna el nuevo arreglo.
-   // Tu código:
+  // Debes iterar sobre el arreglo, y cada elemento pasárselo como arguemento al callback.
+  // Tiene que guardar el resultado devuelto por el callback en cada elemento dentro de un nuevo arreglo.
+  // Retorna el nuevo arreglo.
+  // Tu código:
+  const nuevoArreglo = [];
+  array.forEach((value) => {
+    let retorno = cb(value);
+    nuevoArreglo.push(retorno);
+  });
+  return nuevoArreglo;
 }
 
 function filter(arrayOfStrings) {
-   // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
-   // Luego retorna un nuevo arreglo con estos elementos.
-   // Tu código:
+  // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
+  // Luego retorna un nuevo arreglo con estos elementos.
+  // Tu código:
+  const arregloNuevo = [];
+  arrayOfStrings.forEach((value) => {
+    if (value.charAt(0) === "a") {
+      arregloNuevo.push(value);
+    }
+  });
+  return arregloNuevo;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
-   mayuscula,
-   invocarCallback,
-   operacionMatematica,
-   sumarArray,
-   forEach,
-   map,
-   filter,
+  mayuscula,
+  invocarCallback,
+  operacionMatematica,
+  sumarArray,
+  forEach,
+  map,
+  filter,
 };
